d048d1dfd47e9109d6196695fafa16d56585f46e
Nayan Biswas <79161411+silentknight2001@users.noreply.github.com>
2023-11-20 03:29:12 +0530

    2nd time adding stop.sh file

--- dev-pro-1/scripts/stop_container.sh
+++ dev-pro-1/scripts/stop_container.sh
@@ -2,5 +2,5 @@
 set -e
 
 containerId=`docker ps | awk -F " " '{print $1}'`
-docker rm -f $containerId
+sudo docker rm -f $containerId
 echo "HI nayan container is we do everything working position"
