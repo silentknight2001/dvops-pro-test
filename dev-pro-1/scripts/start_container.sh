502e7df08f776820083ee7dfc3ad71781e586443
silentknight2001 <silentknight.www@gmail.com>
2024-05-13 18:39:34 +0530

    new commit

--- dev-pro-1/scripts/start_container.sh
+++ dev-pro-1/scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull nayan2001/pro-your-py
+docker pull nayan2001/nayan-falsk-python-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 8000:8000 nayan2001/pro-your-py
+docker run -d -p 8000:8000 nayan2001/nayan-falsk-python-app:latest
