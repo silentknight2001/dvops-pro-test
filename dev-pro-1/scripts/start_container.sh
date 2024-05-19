4137266d6ce1b6481124f4441699c7a11ab9d53e
silentknight2001 <silentknight.www@gmail.com>
2024-05-20 00:51:03 +0530

    error solve

--- dev-pro-1/scripts/start_container.sh
+++ dev-pro-1/scripts/start_container.sh
@@ -2,9 +2,9 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull nayan2001/nayan-falsk-python-app:latest
+docker pull nayan2001/nayan-flask-python-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 8000:8000 nayan2001/nayan-falsk-python-app:latest
+docker run -d -p 8000:8000 nayan2001/nayan-flask-python-app:latest
 
 
