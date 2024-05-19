65cde55ac514bc964983f8191fcc36861e4a837a
silentknight2001 <silentknight.www@gmail.com>
2024-05-20 01:26:38 +0530

    error solved

--- dev-pro-1/scripts/stop_container.sh
+++ dev-pro-1/scripts/stop_container.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 set -e
-
-echo "Starting ApplicationStop script..."
 chmod +x dev-pro-1/scripts/stop_container.sh
-
-CONTAINER_NAME="nayan-flask-python-app"
-echo "Checking if container ${CONTAINER_NAME} is running..."
-
-if [ "$(docker ps -q -f name=${CONTAINER_NAME})" ]; then
-  echo "Stopping container ${CONTAINER_NAME}..."
-  docker stop ${CONTAINER_NAME}
-  echo "Container ${CONTAINER_NAME} stopped successfully."
-else
-  echo "No such container: ${CONTAINER_NAME}"
-fi
-
+containerId=`docker ps | awk -F " " '{print $1}'`
+sudo docker rm -f $containerId
 echo "HI nayan container is we do everything working position"
-echo "ApplicationStop script completed."
